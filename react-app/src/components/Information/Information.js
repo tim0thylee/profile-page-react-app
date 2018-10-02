6d6f43c1cf7d8091efaa049364b7cec278b872b0
Timothy Lee <timothylee43@gmail.com>
2018-10-02 14:45:59 -0700

    basic components completed

--- react-app/src/components/Information/Information.js
+++ react-app/src/components/Information/Information.js
@@ -12,7 +12,7 @@ class Information extends Component {
     }
 
     toggle = () => {
-    this.setState({toggle: !this.state.toggle});
+        this.setState({toggle: !this.state.toggle});
     }
 
     render(){
@@ -30,14 +30,53 @@ class Information extends Component {
                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                         </p>
+                        <button className="more-button" onClick={this.toggle}>More</button>
                     </div>
                 </div>
                 <div className="workout-articles">
                     <div className="workout-heading">
                         <h2>Top Articles</h2>
-                        <button onClick={this.toggle}>See All</button>
+                        <button>See All</button>
                     </div>
                     <div className="workout-list">
+                        <ul>
+                            <li>
+                                <a href="listarticle">
+                                    <div className="list-items list-1">
+                                    </div>
+                                    <p>
+                                        Top 5 Ways to Improve Deadlift Gains
+                                    </p>
+                                </a>
+                            </li>
+                            <li>
+                            <a href="listarticle">
+                                <div className="list-items list-2">
+                                </div>
+                                <p>
+                                    Yoga for Faster Recovery
+                                </p>
+                            </a>
+                            </li>
+                            <li>
+                            <a href="listarticle">
+                                <div className="list-items list-3">
+                                </div>
+                                <p>
+                                    Focusing on the Endgame
+                                </p>
+                            </a>
+                            </li>
+                            <li>
+                            <a href="listarticle">
+                                <div className="list-items list-4">
+                                </div>
+                                <p>
+                                    Meal Prep Ideas and Tips for Optimal Nutrition
+                                </p>
+                            </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
